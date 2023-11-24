72870a041b3460efafa0ebedffe9ad21307862be
ChaoticFS <Magnus.overgaard1711@gmail.com>
2023-11-24 09:03:01 +0100

    Add project files.

---
+++ ServiceTemplate/Models/ExampleDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ServiceTemplate.Models
+{
+    public class ExampleDTO
+    {
+        public int Id { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ ServiceTemplate/Repositories/ServiceRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace ServiceTemplate.Repositories
+{
+    public class ServiceRepository
+    {
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+
+        ServiceRepository (ILogger logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+    }
+}
