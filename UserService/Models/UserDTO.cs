60469618ee92cfa40cc7d13ea7a945df387183b5
ChaoticFS <Magnus.overgaard1711@gmail.com>
2023-12-14 14:04:36 +0100

    updated user dto to include registrationdate, need to implement it on registry as well

--- UserService/Models/UserDTO.cs
+++ UserService/Models/UserDTO.cs
@@ -14,7 +14,6 @@ namespace UserService.Models
         public int? Phone { get; set;}
         public string? Email { get; set;}
         public string? Address { get; set;}
+        public DateTime RegistrationDate { get; set;}
     }
-
-
 }
